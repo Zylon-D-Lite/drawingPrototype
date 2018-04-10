d1476d2713d7bf46576bb09f0118044ce758e24c
Zylon-D-Lite <zylon.d.lite@gmail.com>
2018-04-10 18:11:56 +0630

    Added more functionality for EdgeMaker class

--- source/edgemaker.cpp
+++ source/edgemaker.cpp
@@ -1,5 +1,6 @@
 #include "edgemaker.hpp"
-
+#include <cmath>
+#include <exception>
 const std::vector<std::vector<int>> EdgeMaker::PREWITT_MATRIX_X
 {{
     {1, 0, -1},
@@ -26,12 +27,95 @@ const std::vector<std::vector<int>> EdgeMaker::SOBEL_MATRIX_Y
 }};
 
 EdgeMaker::EdgeMaker
-        (const std::vector<std::vector<int>> i_blur_matrix_x
-            = PREWITT_MATRIX_X,
-        const std::vector<std::vector<int>> i_blur_matrix_y
-            = PREWITT_MATRIX_Y) :
-        default_blur_matrix_x(i_blur_matrix_x),
-        default_blur_matrix_y(i_blur_matrix_y)
+        (const png::image<png::rgb_pixel> & i_image,
+              const std::vector<std::vector<int>> i_blur_matrix_x,
+              const std::vector<std::vector<int>> i_blur_matrix_y,
+              bool i_output_intermediate_images,
+              bool i_output_final_image) :
+
+         red_image(i_image.get_width(), i_image.get_height()),
+         green_image(i_image.get_width(), i_image.get_height()),
+         blue_image(i_image.get_width(), i_image.get_height()),
+         default_blur_matrix_x(i_blur_matrix_x),
+         default_blur_matrix_y(i_blur_matrix_y),
+         output_intermediate_images(i_output_intermediate_images),
+         output_final_image(i_output_final_image)
 {
-    
+    split(i_image);
+}
+
+
+void EdgeMaker::gaussian_blur(png::image<png::gray_pixel>& i_img,
+    unsigned kernal_size, double sigma) {
+    auto filter_matrix = generate_gaussian_matrix(kernal_size, sigma);
+    for (auto r : filter_matrix) {
+        for (auto e : r) {
+            std::cout << e << "\t";
+        }
+        std::cout << std::endl;
+    }
+    auto dummy_pixel = png::gray_pixel(255);
+    int k = kernal_size / 2;
+    //iterate through the pixels
+    for (int y = 0; y < i_img.get_height(); ++y) {
+        for (int x = 0; x < i_img.get_width(); ++x) {
+            double sum = 0.0;
+            //for each pixel, apply gauss filter
+            for (int j = -k; j < (int)kernal_size - k; ++j) {
+                for (int i = -k; i < (int)kernal_size - k; ++i) {
+                    png::gray_pixel neighbour_pixel = dummy_pixel;
+                    if (y + j >= 0 && y + j < i_img.get_height() &&
+                        x + i >= 0 && x + i < i_img.get_width()) {
+                    //catch out of bound accesses
+                        neighbour_pixel = i_img[y+j][x+i];
+                    }
+                    sum += (double)neighbour_pixel * filter_matrix[j + k][i + k];
+                }
+            }
+            i_img[y][x] = lround(sum);
+        }
+    }
+}
+
+void EdgeMaker::make_edges() {
+    red_image.write("/home/ineria/Desktop/normal_red.png");
+    green_image.write("/home/ineria/Desktop/normal_green.png");
+    blue_image.write("/home/ineria/Desktop/normal_blue.png");
+    gaussian_blur(red_image, 5, 1.4);
+    gaussian_blur(green_image, 5, 1.4);
+    gaussian_blur(blue_image, 5, 1.4);
+    red_image.write("/home/ineria/Desktop/blurred_red.png");
+    green_image.write("/home/ineria/Desktop/blurred_green.png");
+    blue_image.write("/home/ineria/Desktop/blurred_blue.png");
+}
+
+std::vector<std::vector<double>> EdgeMaker::generate_gaussian_matrix
+             (unsigned kernal_size, double sigma)
+{
+    int k = kernal_size / 2; //based on kernal_size = (2k + 1)
+    double coefficient =  1 / (2 * M_PI * pow(sigma, 2));
+    double exponent_denominator = 2 * pow(sigma, 2);
+    std::vector<std::vector<double>> ret;
+    for (int j = 0; j < kernal_size; ++j) {
+        std::vector<double> current_row;
+        for (int i = 0; i < kernal_size; ++i) {
+            double exponent_numerator =
+                 pow((i - k), 2)  + pow((j - k), 2);
+            double exponent = exponent_numerator / exponent_denominator;
+            double H_i_j = coefficient * pow(M_E, -exponent);
+            current_row.push_back(H_i_j);
+        }
+        ret.push_back(current_row);
+    }
+    return ret;
+}
+
+void EdgeMaker::split(const png::image<png::rgb_pixel>& i_image) {
+   for (size_t y = 0; y < i_image.get_height(); ++y) {
+        for (size_t x = 0; x < i_image.get_width(); ++x) {
+            red_image[y][x] = i_image[y][x].red;
+            green_image[y][x] = i_image[y][x].green;
+            blue_image[y][x] = i_image[y][x].blue;
+        }
+    }
 }
--- source/edgemaker.hpp
+++ source/edgemaker.hpp
@@ -2,23 +2,54 @@
 #include <png++/png.hpp>
 #include <vector>
 
-
+/** EdgeMaker class is responsible for indentifying edges in
+ *  an image.
+ */
 class EdgeMaker {
 public:
+// Matrices used for edge detection
     static const std::vector<std::vector<int>> SOBEL_MATRIX_X;
     static const std::vector<std::vector<int>> SOBEL_MATRIX_Y;
     static const std::vector<std::vector<int>> PREWITT_MATRIX_X;
     static const std::vector<std::vector<int>> PREWITT_MATRIX_Y;
-    EdgeMaker(const std::vector<std::vector<int>> i_blur_matrix_x,
-              const std::vector<std::vector<int>> i_blur_matrix_y);
 
+//! Constructor
+    EdgeMaker(const png::image<png::rgb_pixel> & i_image,
+              const std::vector<std::vector<int>> i_blur_matrix_x,
+              const std::vector<std::vector<int>> i_blur_matrix_y,
+              bool i_output_intermediate_images,
+              bool i_output_final_image);
+
+//! One liner for making an edge for an image.
     void make_edges();
-    void gaussian_blur(unsigned kernal_size);
+
+/* Blurring the image is needed to reduce noise.
+ * But it also softens the edges so careful calibration
+ * is needed.
+ */
+
+//! Typical implementation for blurring
+    void gaussian_blur(png::image<png::gray_pixel>& i_img,
+    unsigned kernal_size, double sigma);
+//! (Supposedly) better version for edge detection
     void changsha_blur(unsigned kernal_size);
 
 private:
+//! Splits the given RGB image to 3 seperate images, one for each color band
+    void split(const png::image<png::rgb_pixel>& i_image);
+
+    std::vector<std::vector<double>> generate_gaussian_matrix
+             (unsigned kernal_size, double sigma);
+
+    png::image<png::gray_pixel> red_image;
+    png::image<png::gray_pixel> green_image;
+    png::image<png::gray_pixel> blue_image;
+
     const std::vector<std::vector<int>> &
         default_blur_matrix_x;
     const std::vector<std::vector<int>> &
         default_blur_matrix_y;
+
+    bool output_intermediate_images;
+    bool output_final_image;
 };

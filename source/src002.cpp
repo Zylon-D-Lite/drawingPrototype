65495c274c822f88bb7eaf6b1aeeb71da4f657a7
Zylon-D-Lite <zylon.d.lite@gmail.com>
2018-04-12 02:13:28 +0630

    adjusted file to fit changes in EdgeMaker constructor

--- source/src002.cpp
+++ source/src002.cpp
@@ -2,6 +2,6 @@
 
 int main() {
     png::image<png::rgb_pixel> img("/home/ineria/Desktop/powerranger.png");
-    EdgeMaker emm(img, EdgeMaker::PREWITT_MATRIX_X, EdgeMaker::PREWITT_MATRIX_Y, true);
+    EdgeMaker emm(img);
     emm.make_edges(20, 0);
 }

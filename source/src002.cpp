f4f1f89a9f94f0d0db8005864cf2b60aad26a4e6
Zylon-D-Lite <zylon.d.lite@gmail.com>
2018-04-17 13:06:41 +0630

    minor improvements

--- source/src002.cpp
+++ source/src002.cpp
@@ -1,7 +1,7 @@
 #include "edgemaker.hpp"
 #include "linemanager.hpp"
 int main() {
-    png::image<png::rgb_pixel> img("/home/ineria/Desktop/powerranger.png");
+    png::image<png::rgb_pixel> img("/home/ineria/Desktop/fb_re.png");
     EdgeMaker emm(img, "/home/ineria/Desktop/zat.png");
     emm.make_edges(20, 0);
     LineManager lm(emm.get_final_image());

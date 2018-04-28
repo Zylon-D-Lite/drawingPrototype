461e9ad9c5ebcd9c2fbbbb81d647e30fddf874db
Zylon-D-Lite <zylon.d.lite@gmail.com>
2018-04-29 04:34:31 +0630

    deleted obsolete codes

--- source/src002.cpp
+++ source/src002.cpp
@@ -1,9 +1,9 @@
 #include "edgemaker.hpp"
 #include "linemanager.hpp"
 int main() {
-    png::image<png::rgb_pixel> img("/home/ineria/Desktop/fb_re.png");
+    png::image<png::rgb_pixel> img("/home/ineria/Desktop/canvas.png");
     EdgeMaker emm(img, "/home/ineria/Desktop/zat.png");
-    emm.make_edges(20, 0);
+    emm.make_edges(5, 0);
     LineManager lm(emm.get_final_image());
-    lm.output_bash_file("/home/ineria/Desktop/wow.sh", 458, 62);
+    lm.output_bash_file("/home/ineria/Desktop/canvas.sh", 458, 62);
 }

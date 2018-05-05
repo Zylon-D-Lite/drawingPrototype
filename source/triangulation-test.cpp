bd81cfb86a12f413fd478251240a70c03d33671e
Zylon-D-Lite <zylon.d.lite@gmail.com>
2018-05-05 06:52:51 +0630

    triangulation-test(example/delaunay) now outputs a full color HD image

--- source/triangulation-test.cpp
+++ source/triangulation-test.cpp
@@ -5,31 +5,38 @@
 
 
 int main() {
-    png::image<png::rgb_pixel> img(1280, 720);
-    double blue_scale = 256 / 1280.0;
-    for (int y = 0; y < img.get_height(); ++y) {
-        for (int x = 0; x < img.get_width(); ++x) {
-            img[y][x] = png::rgb_pixel(250, 250, lround(x * blue_scale));
-        }
-    }
+    //create a buffer image slighly bigger than the final image
+    png::image<png::rgb_pixel> buffer_img(2200, 1300);
     Delaunay d;
+
+    //randomly create points within buffer image bounds
     std::default_random_engine e(2);
-    std::uniform_int_distribution<> ui1280(0, 1279);
-    std::uniform_int_distribution<> ui720(0, 719);
+    std::uniform_int_distribution<> ui1280(0, 2200);
+    std::uniform_int_distribution<> ui720(0, 1300);
     std::vector<Coordinate<double>> v_points;
-    for (int i = 0; i < 200; ++i) {
+    for (int i = 0; i < 450; ++i) {
         v_points.emplace_back(ui1280(e), ui720(e));
     }
+
+    //triangulate points according to delaunay triangulation
     auto triangles = d.triangulate(v_points);
     Artist a;
-    for (auto t : triangles) {
+    int de = 0;
+    std::uniform_int_distribution<> uiColor(0, 255);
 
-        a.draw(img, t.ab(), png::rgb_pixel(0, 0, 0));
-        a.draw(img, t.bc(), png::rgb_pixel(0, 0, 0));
-        a.draw(img, t.ac(), png::rgb_pixel(0, 0, 0));
+    //color in the triangles
+    for (auto t : triangles) {
+        a.color(buffer_img, t, png::rgb_pixel(uiColor(e), uiColor(e), uiColor(e)));
     }
-    a.draw(img, triangles.front().ab(), png::rgb_pixel(255, 0, 0));
-    a.draw(img, triangles.front().bc(), png::rgb_pixel(255, 0, 0));
-    a.draw(img, triangles.front().ac(), png::rgb_pixel(255, 0, 0));
-    img.write("./delaunay-wallpaper.png");
+
+    //final image
+    png::image<png::rgb_pixel> out_img(1980, 1080);
+    for (int y = 0; y < 1080; ++y) {
+        for (int x = 0; x < 1980; ++x) {
+            out_img[y][x] = buffer_img[y + 109][x + 109];
+        }
+    }
+
+    //write the image
+    out_img.write("./delaunay-wallpaper-color-fullHD.png");
 }
